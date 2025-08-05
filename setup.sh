7cb945b0d8a35f17c754c281296b452562f5e4ca
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2025-08-05 11:59:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f install.yaml
+sleep 10
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
