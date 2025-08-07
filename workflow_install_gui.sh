62f30230e56ae8773c9193da445d343906134abc
surajmohan-m <111342002+surajmohan-m@users.noreply.github.com>
2025-08-07 10:02:35 +0530

    Create workflow_install_gui.sh

---
+++ workflow_install_gui.sh
@@ -0,0 +1,20 @@
+kubectl create ns argo
+kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.6.10/install.yaml
+kubectl -n argo get deploy workflow-controller
+kubectl -n argo get deploy workflow-controller
+kubectl -n argo wait deploy --all --for condition=Available --timeout 2m
+
+kubectl patch deployment \
+  argo-server \
+  --namespace argo \
+  --type='json' \
+  -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": [
+  "server",
+  "--auth-mode=server",
+  "--secure=false"
+]},
+{"op": "replace", "path": "/spec/template/spec/containers/0/readinessProbe/httpGet/scheme", "value": "HTTP"}
+]'
+
+kubectl -n argo rollout status --watch --timeout=600s deployment/argo-server
+kubectl -n argo port-forward --address 0.0.0.0 svc/argo-server 2746:2746 > /dev/null &
